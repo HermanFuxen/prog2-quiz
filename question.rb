2d3f8a6cb9aed57283a94a8fa0a61c4ae51648ac
HermanFuxen <herman.fuxen@elev.ga.ntig.se>
2026-09-14 10:45:36 +0200

    lektion 1 uppgifter

--- question.rb
+++ question.rb
@@ -1,28 +1,39 @@
 class Question
+  
+  attr_reader :prompt, :answer # makrokod => skapr nedansående kod(def prompt \n @prompt \n end) för "@promt" & "@anwser"
+  
   def initialize(prompt, answer)
-    @prompt = prompt
+    raise ArgumentError, "prompt must not be empty" if prompt.empty?
+    raise ArgumentError, "answer must not be empty" if answer.empty?
+
+    @prompt = prompt #"@"" betyder: det här tillhör objektet(som defineras att tillhöra classen Quiestion), inte metoden initialize, viketär varför den kan användas i andra metoddefinitioner.
     @answer = answer
   end
 
-  def prompt
-    @prompt
-  end
 
-  def answer
-    @answer
-  end
+  #def prompt
+  #  @prompt
+  #end
 
-  def answer=(new_answer)
-    @answer = new_answer
-  end
+  #def answer
+  #  @answer
+  #end
+
+  #def answer=(new_answer)
+  #  @answer = new_answer
+  #end
 
   def ask
-    puts prompt
+    puts prompt                               #OBS, prompt är metoden prompt, vilket retunerar @prompt
     gets.chomp
   end
 
   def correct?(reply)
-    reply.strip.downcase == answer.downcase
+    reply.strip.downcase == answer.downcase   #OBS, answer är metoden answer, vilket retunerar @answer
+  end
+
+  def hint
+    @answer[0]
   end
 
   def to_s
--- quiz.rb
+++ quiz.rb
@@ -4,7 +4,13 @@ questions = [
   Question.new("Vad heter huvudstaden i Norge?", "Oslo"),
   Question.new("Vilket år släpptes Ruby 1.0?", "1996"),
   Question.new("Vad svarar 5.class?", "Integer"),
+  Question.new("Vad är 5 + 7", "12"),
+  Question.new("En hund är människans bästa _", "vän"),
 ]
+
+#q = Question.new("test", "Oslo")        # ArgumentError: prompt must not be empty
+#q.answer = "x"                          # NoMethodError: undefined method 'answer='
+#q.answer                        # => "Oslo"   att läsa går bra, det meddelandet finns 
 
 score = 0
 
@@ -14,7 +20,14 @@ questions.each do |q|
     puts "Rätt!"
     score += 1
   else
-    puts "Fel. Rätt svar: #{q.answer}"
+    puts "Fel. Hint: #{q.hint}"
+    reply = q.ask
+    if q.correct?(reply)
+      puts "Rätt!"
+      score += 1
+    else
+      puts "Fel. Rätt svar: #{q.answer}"
+    end
   end
 end
 
